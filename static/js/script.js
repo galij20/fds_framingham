647a4757642e79074d7dcc4df47804898b9a9715
linuxmint <196127646+galij20@users.noreply.github.com>
2025-02-19 22:50:59 +0545

    Optimize model and update files accordingly

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,23 @@ document.getElementById('predictionForm').addEventListener('submit', function (e
     e.preventDefault();
 
     // Get input features
-    const features = document.getElementById('features').value.split(',').map(Number);
+    const features = {
+        male: parseInt(document.getElementById('sex').value),
+        age: parseInt(document.getElementById('age').value),
+        education: parseInt(document.getElementById('education').value),
+        currentSmoker: parseInt(document.getElementById('currentSmoker').value),
+        cigsPerDay: parseInt(document.getElementById('cigsPerDay').value),
+        BPMeds: parseInt(document.getElementById('BPMeds').value),
+        prevalentStroke: parseInt(document.getElementById('prevalentStroke').value),
+        prevalentHyp: parseInt(document.getElementById('prevalentHyp').value),
+        diabetes: parseInt(document.getElementById('diabetes').value),
+        totChol: parseInt(document.getElementById('totChol').value),
+        sysBP: parseInt(document.getElementById('sysBP').value),
+        diaBP: parseInt(document.getElementById('diaBP').value),
+        BMI: parseFloat(document.getElementById('BMI').value),
+        heartRate: parseInt(document.getElementById('heartRate').value),
+        glucose: parseInt(document.getElementById('glucose').value)
+    };
 
     // Send POST request to Flask API
     fetch('http://localhost:5001/predict', {
@@ -10,7 +26,7 @@ document.getElementById('predictionForm').addEventListener('submit', function (e
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ features: features }),
+        body: JSON.stringify({ features: Object.values(features) }),
     })
     .then(response => response.json())
     .then(data => {
